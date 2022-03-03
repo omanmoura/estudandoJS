2aeb2338dc8282845106be76ebdf576332d44a7c
Oman <omanmoura@gmail.com>
2022-03-03 20:14:13 -0300

    adicionando: atribuição de variáveis

---
+++ atribuicaoDeVariaveis.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+//Utilizar o "let" permite que a variável seja sobrescrita futuramente. Funciona bem para "contadores"
+//TENTAR REDUZIR AO MÁXIMO A UTILIZAÇÃO!!!
+
+const idade = 26;
+let primeiroNome = "Oman";
+const sobrenome = "Moura";
+
+console.log(primeiroNome + " " + sobrenome);
+
+//Forma mais enxuta para concatenar
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`)
+
+const nomeCompleto = primeiroNome + sobrenome;
+
+console.log(nomeCompleto)
