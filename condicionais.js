b1f94de8173eb54df4aff3d8ce24ff299f3be5d3
Oman <omanmoura@gmail.com>
2022-03-04 10:50:55 -0300

    Adicionando funcionalidades

--- condicionais.js
+++ condicionais.js
@@ -6,24 +6,18 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 18;
+const idadeComprador = 15;
 const estaAcompanhado = 0;
 
 console.log("\nDestinos possíveis:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("\nComprador maior de idade");
+if (idadeComprador >= 18 || estaAcompanhado) {
+    console.log("\nUsuário habilitado para comprar.");
     console.log(`\nDestino vendido: ${listaDeDestinos[1]}`)
     listaDeDestinos.splice(1,1);
 } else {
-    if (estaAcompanhado) {
-        console.log("\nComprador menor de idade acompanhado");
-        console.log(`Destino vendido: ${listaDeDestinos[1]}`)
-        listaDeDestinos.splice(1,1);
-    } else {
-        console.log("Comprador menor de idade e não está acompanhado");
-    }
+    console.log("\nComprador menor de idade e não está acompanhado. Não pode comprar.");
 }
 
 console.log("\nDestinos disponíveis:");
